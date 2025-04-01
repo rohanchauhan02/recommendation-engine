54b0d30f7fcd5c9e82d0c65b3473ea7532803e45
Rohan Chauhan <rohan.singh@qoala.id>
2025-04-01 14:22:18 +0530

    Automated fix for: 'The issue occurs because AddMedicine() doesn't implemented. Modify the function AddMedicine() to make the service working'

--- modules/medicine/usecase/usecase.go
+++ modules/medicine/usecase/usecase.go
@@ -1,20 +1,3 @@
-package usecase
-
-import (
-	dto "github.com/rohanchauhan02/recommendation-engine/dto/medicine"
-	"github.com/rohanchauhan02/recommendation-engine/modules/medicine"
-)
-
-type usecase struct {
-	repository medicine.Repository
-}
-
-func NewMedicineUsecase(repository *medicine.Repository) medicine.Usecase {
-	return &usecase{
-		repository: *repository,
-	}
-}
-
-func (u *usecase) AddMedicine(req *dto.CreateMedicineRequest) error {
-	return u.repository.AddMedicine(req)
+type MedicineUsecase interface {
+    AddMedicine(medicine *domain.Medicine) error
 }
